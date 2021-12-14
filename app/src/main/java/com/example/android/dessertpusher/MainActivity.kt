a5f188285bce6067e35018ff80560463a2350fd8
Rakib12320416 <rakib.uzzaman1232@gmail.com>
2021-12-14 19:28:24 +0600

    Add Timber log

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -74,7 +74,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Timber.i("onCreate Called")
-        Log.i("MainActivity", "onCreate called")
 
         // Use Data Binding to get reference to the views
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -192,7 +191,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onStart() {
         super.onStart()
         Timber.i("onStart Called")
-        Log.i("MainActivity", "onCreate called")
     }
 
     override fun onResume() {
